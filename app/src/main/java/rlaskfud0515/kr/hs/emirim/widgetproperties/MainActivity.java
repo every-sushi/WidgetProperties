ffd407e89cfad5ea7764671d60f5713f54a5f9eb
Yeonjae Woo <yeonjae0331@naver.com>
2017-03-21 19:07:53 +0900

    first commit

---
+++ app/src/main/java/rlaskfud0515/kr/hs/emirim/widgetproperties/MainActivity.java
@@ -0,0 +1,15 @@
+package rlaskfud0515.kr.hs.emirim.widgetproperties;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+}
+
+
